494c79b6e699bb81f16580dcb1ec28e9b1f80488
ruibin.chow <ruibin.chow@qq.com>
2017-08-11 11:23:20 +0800

    * 修改数据TIMESTAMP字段设置为当前时间，但以后修改时，不再刷新它 * 完成修改个人信息接口

--- creaction/service/account/changeInfo.py
+++ creaction/service/account/changeInfo.py
@@ -8,7 +8,7 @@
 # 
 
 """
-
+修改个人信息
 """
 from service.account import account
 from flask import Flask, Response, request
@@ -22,9 +22,62 @@ from common.tools import getValueFromRequestByKey
 
 
 @account.route('/change_info', methods=["POST"])
-# @vertifyTokenHandle
+@vertifyTokenHandle
 def changeInfo():
+    userUUID = getValueFromRequestByKey("user_uuid")
+    userdata = getValueFromRequestByKey("userdata")
+    
+    userDict = None
+    try:
+        userDict = json.loads(userdata)
+    except Exception as e:
+        Loger.error(e, __file__)
+        return RESPONSE_JSON(CODE_ERROR_ILLEGAL_DATA_CONTENT)
     pass
+
+    if len(userDict) < 6:
+        return RESPONSE_JSON(CODE_ERROR_PARAM)
+
+    if changeUserInfoInStorage(userUUID, userDict):
+        return RESPONSE_JSON(CODE_SUCCESS)
+    else:
+        return RESPONSE_JSON(CODE_ERROR_SERVICE)
+
+
+def changeUserInfoInStorage(userUUID, userDict):
+    result = False
+    excuteSQLList = []
+    
+    deleteSQL = """DELETE FROM t_user_eduction WHERE user_uuid='%s'""" % userUUID
+    excuteSQLList.append(deleteSQL)
+
+    schoolList = userDict["school"]
+    if len(schoolList) != 0:
+        insertSQL = """INSERT INTO t_user_eduction (user_uuid, sorting, school) VALUES """
+        values = ""
+        schoolListNum = len(schoolList)
+        for i in range(schoolListNum):
+            tempString = """('%s', %d, '%s')""" % (userUUID, i, schoolList[i])
+            values += tempString + ","
+        values = values[:-1] + ";"
+        insertSQL += values
+        excuteSQLList.append(insertSQL)
+
+    updateSQL = """
+        UPDATE t_user SET nickname='%s', gender=%d, area='%s', detail='%s', career='%s'
+        WHERE uuid='%s'; """ % (userDict["nickname"], int(userDict["gender"]), 
+        userDict["area"], userDict["detail"], userDict["career"], userUUID)
+    excuteSQLList.append(updateSQL)
+
+    dbManager = DB.DBManager.shareInstanced()
+    try: 
+            result = dbManager.executeTransactionMutltiDml(excuteSQLList)
+    except Exception as e:
+            Loger.error(e, __file__)
+            
+    return result
+    
+
 
 
 
--- tests/account/test-changeInfo.py
+++ tests/account/test-changeInfo.py
@@ -10,6 +10,26 @@
 """
 
 """
+import requests
+
+userdata = """
+{
+        "nickname":"用户昵称2121212133",
+        "gender":0,
+        "area":"坐标",
+        "detail":"简介",
+        "school":["学校1", "学校2", "学校3", "学校244444"],
+        "career":"职业"
+}
+"""
+# userdata =""
+cookies = {"token": "MTUwMjI2ODE4OS45NjpMVndDZDFodXFMeU9MYUhsSVlsbUpPVDIzSDA9Cg=="}
+params = {"user_uuid": "f6e996f8-7d83-11e7-889b-000000", "userdata":userdata}
+r = requests.post(url="http://127.0.0.1:5000/service/account/change_info", params=params, cookies=cookies)    
+
+print r.cookies
+print(r.text)
+
 
 
 if __name__ == '__main__':
