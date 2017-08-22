b8dd3d8937285bd27eee83bb092140661e6a6887
ruibin.chow <ruibin.chow@qq.com>
2017-08-22 12:22:55 +0800

    加入项目时同时与作者彼此交换了联系方式

--- creaction/service/project/inviteAndAgreeJoinProject.py
+++ creaction/service/project/inviteAndAgreeJoinProject.py
@@ -65,6 +65,8 @@ def invitePeopleIntoProject(userUUID, projectUUID, memberUUID):
 def agreeIntoProject(userUUID, projectUUID):
     if userUUID == None or projectUUID==None:
         return RESPONSE_JSON(CODE_ERROR_MISS_PARAM) 
+    
+    sqlList = []
 
     # 成为项目成员同时关注该项目
     insertProjectUserSQL = """
@@ -74,6 +76,7 @@ def agreeIntoProject(userUUID, projectUUID):
     """.format(projectUUID=projectUUID, userUUID=userUUID, 
                 memberType=Config.TYPE_FOR_PROJECT_MEMBER, 
                 followType=Config.TYPE_FOR_PROJECT_FOLLOWER)
+    sqlList.append(insertProjectUserSQL)
 
     # 消息中action状态改为已执行即on 
     updateMessageSQL = """
@@ -81,10 +84,15 @@ def agreeIntoProject(userUUID, projectUUID):
         WHERE user_uuid='%s' AND content_uuid='%s' AND type=%d;
     """ % (Config.TYPE_FOR_MESSAGE_ACTION_ON, userUUID, projectUUID, 
                 Config.TYPE_FOR_MESSAGE_IN_PROJECT_FOR_INVITE)
+    sqlList.append(updateMessageSQL)
 
     dbManager = DB.DBManager.shareInstanced()
     try:
-        dbManager.executeTransactionMutltiDml([insertProjectUserSQL, updateMessageSQL])
+        # 加入项目时查看跟作者是否已经交换联系方式了，没有就双双交换
+        exchangeContactSQL = exchangeContactSQLMethod(userUUID, projectUUID)
+        if exchangeContactSQL != None: sqlList.append(exchangeContactSQL)
+
+        dbManager.executeTransactionMutltiDml(sqlList)
     except Exception as e:
         Loger.error(e, __file__)
         return RESPONSE_JSON(CODE_ERROR_SERVICE)
@@ -96,6 +104,33 @@ def __notificationMember(userUUID):
     content = "邀请你加入项目"
     notification.notificationUserForContent(userUUID, content)
     # dispatchNotificationUserForContent.delay(userUUID)
+
+
+def exchangeContactSQLMethod(userUUID, projectUUID):
+    queryAuthorSQL = """SELECT author_uuid FROM t_project WHERE uuid='%s';""" % projectUUID
+    dbManager = DB.DBManager.shareInstanced()
+    try:
+        result = dbManager.executeTransactionQuery(queryAuthorSQL)
+        authorUUID = result[0]["author_uuid"]
+
+        queryContactSQL = """
+            SELECT user_uuid FROM t_user_user WHERE
+            user_uuid='{authorUUID}' AND other_user_uuid='{userUUID}' AND type={typeStr} GROUP BY user_uuid
+        """.format(authorUUID=authorUUID, typeStr=Config.TYPE_FOR_USER_CONTACT, 
+                        userUUID=userUUID)
+        result = dbManager.executeTransactionQuery(queryContactSQL)
+        if len(result) == 0:
+            insertContactSQL = """INSERT INTO t_user_user (user_uuid, type, other_user_uuid) 
+                VALUES ('{userUUID}', {typeInt}, '{authorUUID}'), ('{authorUUID}', {typeInt},
+                '{userUUID}'); """.format(userUUID=userUUID, 
+                typeInt=Config.TYPE_FOR_USER_CONTACT, authorUUID=authorUUID)
+            return insertContactSQL
+        else:
+            return None
+    except Exception as e:
+        Loger.error(e, __file__)
+        raise e
+
 
 
 if __name__ == '__main__':
