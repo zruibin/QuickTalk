c6fa76099fe000a7e668171d79b154d84e18b801
ruibin.chow <ruibin.chow@qq.com>
2017-09-21 15:59:52 +0800

    查询日志列表三表查询关联sql错误问题

--- creaction/service/project/journalList.py
+++ creaction/service/project/journalList.py
@@ -38,30 +38,44 @@ def __queryJournalFromStorage(projectUUID, index, userUUID):
     querySQL = ""
     if userUUID == None:
         querySQL = """
-            SELECT t_project_journal.uuid AS journal_uuid, t_project_journal.project_uuid, user_uuid, t_project_journal.time, `like`, content,
-            t_user.nickname, t_user.avatar,
-            t_project_journal_media.media_name, t_project_journal_media.sorting
-            FROM t_project_journal, t_user, t_project_journal_media
-            WHERE t_project_journal.project_uuid='{projectUUID}' 
-                AND t_user.uuid=t_project_journal.user_uuid
-                AND t_project_journal_media.journal_uuid=t_project_journal.uuid 
-                AND  t_project_journal_media.type={typeStr}
-            ORDER BY t_project_journal.time DESC {limitStr};
+            SELECT t_temp.journal_uuid, t_temp.project_uuid, user_uuid, t_temp.time, `like`,content, nickname, avatar, t_project_journal_media.media_name, t_project_journal_media.sorting FROM 
+            (
+            SELECT t_project_journal.uuid AS journal_uuid, 
+                    t_project_journal.project_uuid, 
+                    t_project_journal.user_uuid, 
+                    t_project_journal.time, `like`, content,
+                    t_user.nickname, t_user.avatar
+                    FROM t_project_journal, t_user
+                    WHERE t_project_journal.project_uuid='{projectUUID}' 
+                        AND t_user.uuid=t_project_journal.user_uuid
+            ) AS t_temp 
+            LEFT JOIN t_project_journal_media 
+            ON t_project_journal_media.journal_uuid=t_temp.journal_uuid
+                        AND  t_project_journal_media.type={typeStr}
+            ORDER BY t_temp.time DESC {limitStr};
         """.format(projectUUID=projectUUID, limitStr=limitStr,
                 typeStr=Config.TYPE_FOR_PROJECT_MEDIAS_PICTURE)
     else:
         querySQL = """
-            SELECT t_project_journal.uuid AS journal_uuid, t_project_journal.project_uuid, user_uuid, t_project_journal.time, `like`, content,
-            (SELECT count(content_uuid) FROM t_like 
-                WHERE content_uuid=t_project_journal.uuid AND user_uuid='{userUUID}' AND type='{likeType}') AS likeStatus,
-            t_user.nickname, t_user.avatar,
-            t_project_journal_media.media_name, t_project_journal_media.sorting
-            FROM t_project_journal, t_user, t_project_journal_media
-            WHERE t_project_journal.project_uuid='{projectUUID}' 
-                AND t_user.uuid=t_project_journal.user_uuid
-                AND t_project_journal_media.journal_uuid=t_project_journal.uuid 
-                AND  t_project_journal_media.type={typeStr}
-            ORDER BY t_project_journal.time DESC {limitStr};
+            SELECT t_temp.journal_uuid, t_temp.project_uuid, user_uuid, t_temp.time, `like`,content, likeStatus, nickname, avatar, t_project_journal_media.media_name, t_project_journal_media.sorting FROM 
+            (
+            SELECT t_project_journal.uuid AS journal_uuid, 
+                    t_project_journal.project_uuid, 
+                    t_project_journal.user_uuid, 
+                    t_project_journal.time, `like`, content,
+                    (SELECT count(content_uuid) FROM t_like 
+                        WHERE content_uuid=t_project_journal.uuid 
+                        AND user_uuid='{userUUID}' AND type='{likeType}'
+                    ) AS likeStatus,
+                    t_user.nickname, t_user.avatar
+                    FROM t_project_journal, t_user
+                    WHERE t_project_journal.project_uuid='{projectUUID}' 
+                        AND t_user.uuid=t_project_journal.user_uuid
+            ) AS t_temp 
+            LEFT JOIN t_project_journal_media 
+            ON t_project_journal_media.journal_uuid=t_temp.journal_uuid
+                        AND  t_project_journal_media.type={typeStr}
+            ORDER BY t_temp.time DESC {limitStr};
         """.format(projectUUID=projectUUID, limitStr=limitStr,
                 typeStr=Config.TYPE_FOR_PROJECT_MEDIAS_PICTURE, userUUID=userUUID, likeType=Config.TYPE_FOR_LIKE_IN_JOURNAL)
 
