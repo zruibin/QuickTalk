70d3651edc9f143d77a2baae0e0f14d38d525c08
ruibin.chow <ruibin.chow@qq.com>
2018-01-12 20:22:45 +0800

    RecycleView数据

--- app/src/main/java/com/creactism/quicktalk/services/userpost/RecommendFragment.java
+++ app/src/main/java/com/creactism/quicktalk/services/userpost/RecommendFragment.java
@@ -6,20 +6,35 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.creactism.quicktalk.R;
 import com.creactism.quicktalk.components.Navigationbar;
+import com.creactism.quicktalk.components.RecycleViewDivider;
 import com.creactism.quicktalk.util.DLog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by ruibin.chow on 12/01/2018.
  */
 
 public class RecommendFragment extends Fragment {
+
+//    @BindView(R.id.recommend_recyclerview)
+    public RecyclerView recyclerView;
+    private List<String> mDatas;
+    private RecommendAdapter mAdapter;
 
     @Override
     public void onAttach(Context context) {
@@ -39,6 +54,7 @@ public class RecommendFragment extends Fragment {
 //        DLog.debug(    "RecommendFragment on onCreateView: .....");
 
         View view = inflater.inflate(R.layout.fragment_recommend, container, false);
+//        ButterKnife.bind(view);
 
         Navigationbar navigationbar = (Navigationbar)view.findViewById(R.id.recommend_navigationbar);
         navigationbar.setTitle("第二项");
@@ -62,6 +78,16 @@ public class RecommendFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+
+        initData();
+        this.recyclerView = (RecyclerView)view.findViewById(R.id.recommend_recyclerview);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        this.recyclerView.setAdapter(mAdapter = new RecommendAdapter());
+        this.recyclerView.addItemDecoration(new RecycleViewDivider(
+                getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, 10, getResources().getColor(R.color
+                .gray)));
+        this.mAdapter.notifyItemChanged(2);
 
         return view;
 //        return super.onCreateView(inflater, container, savedInstanceState);
@@ -119,5 +145,55 @@ public class RecommendFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
 //        DLog.debug(    "RecommendFragment on onDetach: ...");
+    }
+
+
+    protected void initData()
+    {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++)
+        {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.MyViewHolder>
+    {
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                    getActivity()).inflate(R.layout.fragment_recommend_item, parent,
+                    false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+            holder.tv.setTag(new Integer(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView tv;
+            public MyViewHolder(View view) {
+                super(view);
+                tv = (TextView)view.findViewById(R.id.id_num);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Integer index = (Integer)v.getTag();
+                        String indexStr = "item: " + String.valueOf(index.intValue());
+                        DLog.info(indexStr);
+                        Toast.makeText(getActivity().getApplicationContext(), indexStr, Toast
+                                .LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
     }
 }
