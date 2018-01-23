6df6986cc55282bda4f3a4e1d3e12e3b23e830e8
ruibin.chow <ruibin.chow@qq.com>
2018-01-23 16:55:15 +0800

    完成第三方同帐号管理UI

--- app/src/main/java/com/creactism/quicktalk/services/account/AccountThirdPartActivity.java
+++ app/src/main/java/com/creactism/quicktalk/services/account/AccountThirdPartActivity.java
@@ -1,6 +1,5 @@
 package com.creactism.quicktalk.services.account;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,8 +11,10 @@ import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.creactism.quicktalk.BaseActivity;
+import com.creactism.quicktalk.R;
 import com.creactism.quicktalk.components.tableview.TableEntity;
 import com.creactism.quicktalk.components.tableview.TableListAdapter;
+import com.creactism.quicktalk.util.Tools;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ import java.util.List;
  * Created by ruibin.chow on 23/01/2018.
  */
 
-public class AccountSettingActivity extends BaseActivity {
+public class AccountThirdPartActivity extends BaseActivity {
 
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
@@ -33,7 +34,7 @@ public class AccountSettingActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setTitle("帐号与安全");
+        this.setTitle("第三方帐号管理");
         this.navigationBar.setDefaultBackAction(this);
 
         this.recyclerView = new RecyclerView(this);
@@ -48,21 +49,21 @@ public class AccountSettingActivity extends BaseActivity {
         this.recyclerView.setLayoutManager(this.layoutManager);
         this.recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
-        this.sectionTableListAdapter = new AccountSettingAdapter(this.dataList);
+        this.sectionTableListAdapter = new AccountThirdPartAdapter(this.dataList);
         this.recyclerView.setAdapter(this.sectionTableListAdapter);
     }
 
     private void loadData() {
         this.dataList.add(new TableEntity(true, "", new TableEntity.IndexPath(0, 0)));
-        this.dataList.add(new TableEntity("修改手机号码", new TableEntity.IndexPath(0, 0)));
-        this.dataList.add(new TableEntity("修改密码", new TableEntity.IndexPath(0, 1)));
-        this.dataList.add(new TableEntity("第三方帐号管理", new TableEntity.IndexPath(0, 2)));
+        this.dataList.add(new TableEntity("", new TableEntity.IndexPath(0, 0)));
+        this.dataList.add(new TableEntity("", new TableEntity.IndexPath(0, 1)));
+        this.dataList.add(new TableEntity("", new TableEntity.IndexPath(0, 2)));
     }
 
 
-    private class AccountSettingAdapter extends TableListAdapter {
+    private class AccountThirdPartAdapter extends TableListAdapter {
 
-        public AccountSettingAdapter(List<TableEntity> data) {
+        public AccountThirdPartAdapter(List<TableEntity> data) {
             super(data);
         }
 
@@ -70,25 +71,36 @@ public class AccountSettingActivity extends BaseActivity {
         public void cellForRowAtIndexPath(final BaseViewHolder helper, final TableEntity item, final TableEntity
                 .IndexPath indexPath) {
 
+//            ImageView indicator = helper.getView(table_indicator_id);
+//            indicator.setVisibility(View.GONE);
+
             final SimpleDraweeView imageView = helper.getView(this.table_image_id);
-            imageView.setVisibility(View.GONE);
-
+            imageView.setPadding(4, 4, 4, 4);
             TextView textView = helper.getView(this.table_text_id);
-            textView.setText((String)item.getObj());
-
-            ImageView indicator = helper.getView(table_indicator_id);
-            if (indexPath.section == 1 && indexPath.row == 4) {
-                indicator.setVisibility(View.GONE);
+            textView.setPadding(10, 0, 0, 0);
+            TextView detailView = helper.getView(this.table_detail_id);
+            if (indexPath.row == 0) {
+                imageView.setImageURI(Tools.getResoucesUri(R.mipmap.wechat_bind));
+                textView.setText("微信");
+                detailView.setText("已绑定");
             }
+            if (indexPath.row == 1) {
+                imageView.setImageURI(Tools.getResoucesUri(R.mipmap.qq_bind));
+                textView.setText("QQ");
+                detailView.setText("已绑定");
+            }
+            if (indexPath.row == 2) {
+                imageView.setImageURI(Tools.getResoucesUri(R.mipmap.weibo_bind));
+                textView.setText("微博");
+                detailView.setText("已绑定");
+            }
+
         }
 
         @Override
         public void didSelectRowAtIndexPath(final TableEntity item, final TableEntity.IndexPath indexPath) {
 
-            if (indexPath.row == 2) {
-                Intent intent = new Intent().setClass(getBaseContext(), AccountThirdPartActivity.class);
-                startActivity(intent);
-            }
+
         }
     }
 }
